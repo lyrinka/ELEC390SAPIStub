b8642aadc7d5b201417d266e3b75d6b87a80933b
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-07-17 14:46:30 +0800

    Suppression of unused warnings on event handlers

--- app/src/main/java/app/uvtracker/sensor/pii/event/EventHandler.java
+++ app/src/main/java/app/uvtracker/sensor/pii/event/EventHandler.java
@@ -6,4 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
 
+    // TODO: any way to make this suppress unused warning in native Java?
+
 }
